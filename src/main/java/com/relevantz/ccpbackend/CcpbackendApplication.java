7a4ad3a4a75813f8faa2d9314eb98d7586da8a24
OFS1\vinoth.moorthi <vinoth.moorthi@relevantz.com>
2024-03-04 15:47:52 +0530

    Initial commit

---
+++ src/main/java/com/relevantz/ccpbackend/CcpbackendApplication.java
@@ -0,0 +1,13 @@
+package com.relevantz.ccpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcpbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcpbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relevantz/ccpbackend/CcpbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevantz.ccpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcpbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
